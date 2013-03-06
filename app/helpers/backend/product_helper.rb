0e2eb825d6ad262c6aa91e4f9161db030f80313a
Fer Perales <fer.perales.montoya@gmail.com>
2013-03-06 13:43:09 -0600

    Replace infinite message to infinite symbol

--- app/helpers/backend/product_helper.rb
+++ app/helpers/backend/product_helper.rb
@@ -1,7 +1,7 @@
 module Backend::ProductHelper
 
 	def display_inventory product
-		product.inventory == -1? 'Infinite' : product.inventory
+		product.inventory == -1? '∞' : product.inventory
 	end
 
 end
