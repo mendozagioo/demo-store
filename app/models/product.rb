25b32983b0390193dfbd35fde3be51649877ba9c
Fer Perales <fer.perales.montoya@gmail.com>
2013-03-06 11:34:15 -0600

    Add validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,13 @@
 class Product < ActiveRecord::Base
+
+	belongs_to :admin
+	has_many :photos
+  validate_on_create :photos_count_within_bounds
+
+  private
+  def photos_count_within_bounds
+    return if photos.blank?
+    errors.add("Too many photos") if photos.length > 10
+  end
+  
 end
